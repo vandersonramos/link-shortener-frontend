6ef737cee7407eac4d9180c7a63207dadee44649
Vanderson Ramos <ramos.vandersoninfor@gmail.com>
2019-04-27 20:16:38 -0300

    strutucture

--- link-shortener/src/router/index.js
+++ link-shortener/src/router/index.js
@@ -1,15 +1,38 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Main from '@/modules/Main/views/Main';
+import Success from '@/modules/Main/views/Success';
+import Decrypt from '@/modules/Main/views/Decrypt';
+import Analyze from '@/modules/Main/views/Analyze';
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
   routes: [
     {
+      path: '*',
+      redirect: '/'
+    },
+    {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Main',
+      component: Main
+    },
+    {
+      path: '/success',
+      name: 'Success',
+      component: Success
+    },
+    {
+      path: '/:url_key',
+      name: 'Decrypt',
+      component: Decrypt
+    },
+    {
+      path: '/views/analyze',
+      name: 'Analyze',
+      component: Analyze
     }
   ]
 })
